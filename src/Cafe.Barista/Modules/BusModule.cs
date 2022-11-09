5ded5010c36f4a4650fee5c2a71feda4ee3f9e11
Damian Maclennan <damian@damianm.com>
2022-11-09 22:22:52 +1000

    init again

---
+++ src/Cafe.Barista/Modules/BusModule.cs
@@ -0,0 +1,45 @@
+using System;
+using Autofac;
+using Cafe.Messages;
+using Nimbus.Configuration;
+using Nimbus.Infrastructure;
+using Nimbus.InfrastructureContracts;
+using Nimbus.Logger.Serilog.Configuration;
+using Nimbus.Serializers.Json.Configuration;
+using Nimbus.Transports.Redis;
+
+namespace Barista.Modules
+{
+    public class BusModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            var handlerTypesProvider = new AssemblyScanningTypeProvider(typeof(BusModule).Assembly, typeof(PlaceOrderCommand).Assembly);
+
+            builder.RegisterNimbus(handlerTypesProvider);
+            builder.Register(componentContext => new BusBuilder()
+                                                 .Configure()
+                                                 .WithTransport(
+                                                     new RedisTransportConfiguration().WithConnectionString("localhost")
+                                                     
+                                                     )
+
+                                                 .WithNames("Barista", Environment.MachineName)
+                                                 .WithTypesFrom(handlerTypesProvider)
+                                                 .WithAutofacDefaults(componentContext)
+                                                 .WithSerilogLogger()
+                                                 .WithJsonSerializer()
+//                                                 .WithGlobalInboundInterceptorTypes(typeof(CorrelationIdInterceptor))
+                                                 .Build())
+                   .As<IBus>()
+                   .AutoActivate()
+                   .OnActivated(c => c.Instance.Start())
+                   .OnRelease(bus =>
+                              {
+                                  bus.Stop().Wait();
+                                  bus.Dispose();
+                              })
+                   .SingleInstance();
+        }
+    }
+}
